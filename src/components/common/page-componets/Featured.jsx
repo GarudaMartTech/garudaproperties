3bd364e18fad05dce189968bee52b39b64b4ec1d
GarudaMartTech <garudamarttech@gmail.com>
2025-07-12 12:25:28 +0530

    add image

--- src/components/common/page-componets/Featured.jsx
+++ src/components/common/page-componets/Featured.jsx
@@ -8,7 +8,7 @@ const Featured = () => {
         {/* <h1 className="mx-auto sub-heading">featured</h1> */}
         <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">Proposed Construction and Development Projects</h1>
       </div>
-      <div className="grid grid-cols-2  gap-4 mt-8">
+      <div className="grid grid-cols-3   gap-4 mt-8">
         {property.slice(0, 3).map((featured) => (
           <SingleProductCard key={featured.id} {...featured} />
         ))}
